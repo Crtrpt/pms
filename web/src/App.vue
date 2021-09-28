48044429f18121d0b1cc76d8db8396310e75d7d8
jzx <945627077@qq.com>
2021-09-28 12:37:35 +0800

    '前端项目初始化'

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
